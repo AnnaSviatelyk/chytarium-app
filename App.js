6f87120328d06009b583e447eb9503f00168bdc8
Anna Sviatelyk <anna.sviatelyk@gmail.com>
2023-03-02 12:15:24 +0400

    feat(app): added tailwindcss and typescript

--- App.js
+++ App.js
@@ -3,8 +3,8 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View className="flex bg-red">
+      <Text>Welcome to Chytarium!</Text>
       <StatusBar style="auto" />
     </View>
   );
