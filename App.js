0fe543896711993f7ec81fbc506c41885c164814
Anna Sviatelyk <anna.sviatelyk@gmail.com>
2023-03-02 14:01:45 +0400

    feat(app): added linter configs

--- App.js
+++ App.js
@@ -1,20 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Text, View } from "react-native";
 
 export default function App() {
   return (
-    <View className="flex bg-red">
+    <View className="flex-1 items-center justify-center bg-yellow-100 ">
       <Text>Welcome to Chytarium!</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
